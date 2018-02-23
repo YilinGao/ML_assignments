8d9b338415ee3742e07ded6d7f55b898dacd9b4f
Yilin Gao <yilin.gao@duke.edu>
2018-02-23 15:26:25 -0500

    q4 minor update

--- hw4/hw4_code/svm_classifier.py
+++ hw4/hw4_code/svm_classifier.py
@@ -4,7 +4,7 @@ from cvxopt import matrix, solvers
 
 class SVMClassifierError(Exception):
     """Base calss for exception in SVMClassifier module
-    
+
     Attributes:
         message: the error message
     """
@@ -20,17 +20,17 @@ class ClassifierUndefinedError(SVMClassifierError):
     raised only in a hard SVM classifier predict() method
     """
     pass
-    
+
 class SVMClassifier:
     """A hard SVM classifier using user defined kernel functions"""
     def __init__(self, kernel):
         """__init__ for SVMClassifier class
-        
+
         Attributes:
             kernel(x1, x2): the kernel function that take 2 x observations
         """
         # if the hard constrained SVM is feasible
-        self.infeasible = True
+        self.undefined = True
         # primal variables
         self.lambdas = None
         self.lambda_zero = None
@@ -43,12 +43,12 @@ class SVMClassifier:
         """Method to train the SVM classifier
         Takes features (X) and labels(y), uses a quadratic solver (cvxopt) to solve the dual primal problem,
         uses the optimal dual variables to compute primal variables.
-        If the data is not separable, raise an exception, set self.infeasible to False, and leave optimal variables as None
-        
+        If the data is not separable, raise an exception, set self.undefined to False, and leave optimal variables as None
+
         Attributes:
             X: numpy.ndarray, shape = [n, p]
             y: numpy.ndarray, shape = [n, 1]
-            
+
         Exceptions:
             DataNonSeparableError: raised when the training data is not separable
         """
@@ -63,7 +63,7 @@ class SVMClassifier:
         for i in range(n):
             for j in range(n):
                 gram[i, j] = self.kernel(X[i, :], X[j, :])
-        
+
         P = matrix(np.outer(y, y) * gram)
         q = matrix(np.ones(n) * -1)
         G = matrix(np.diag(np.ones(n) * -1))
@@ -72,13 +72,13 @@ class SVMClassifier:
         b = matrix(0.00)
         solvers.options['show_progress'] = False # make the solver work silently
         sol=solvers.qp(P, q, G, h, A, b)
-        
+
         # the quadratic solver cannot find optimal solution to the problem
         # so the training data is not separable
         if sol['status'] != 'optimal':
             raise DataNonSeparableError('The training data is not separable. The HARD SVM classifier has failed!')
-        
-        self.infeasible = False
+
+        self.undefined = False
         self.multipliers = np.array(sol['x'])
         self.multipliers = self.multipliers.flatten() # turn it into 1D array
 
@@ -93,26 +93,26 @@ class SVMClassifier:
             self.lambdas += multiplier * yi * Xi
         # compute primal variable lambda_zero
         self.lambda_zero = 1 - np.dot(self.lambdas, X_sv[0])
-        
+
     def predict(self, X):
         """Method to predict labels based on classifier parameters
-        Takes features (X), uses classifier primal variables to work out f(x), 
+        Takes features (X), uses classifier primal variables to work out f(x),
         and uses sign(f(x)) to predict labels
-        
+
         Attributes:
             X: numpy.ndarray, shape = [n, p]
-        
+
         Return values:
             yhat: numpy.ndarray, shape = [n, 1], each element can take 2 possible values {0, 1}
-        
+
         Exceptions:
-            ClassifierUndefinedError: raised when the classifier status is infeasible
+            ClassifierUndefinedError: raised when the classifier status is undefined
         """
         # the training data is not separable so the classifier parameters are not defined
         # cannot do the prediction
-        if self.infeasible:
+        if self.undefined:
             raise ClassifierUndefinedError('The SVM classifier is not defined. Please train it with separable data!')
-        
+
         n, p = X.shape
         y_hat = np.zeros(n)
         y_hat = self.lambda_zero + np.dot(X, self.lambdas.T)
